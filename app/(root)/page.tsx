55749890d502080505bb22bee432194af10c44bc
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-09-18 16:47:39 -0600

    Refactor sign-in and sign-up pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
+      <div className="mr-4">
+      <UserButton afterSwitchSessionUrl="/" />
+      </div>
       <h1 className="text-4xl font-bold">Hello, World!</h1>
     </div>
   );
