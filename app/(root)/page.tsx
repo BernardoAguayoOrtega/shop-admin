b4965b2951e42f0d9bab265e9b2688ac58840967
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-09-26 18:18:41 -0600

    Refactor Home page and add Modal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,10 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="p-4">
       <div className="mr-4">
-      <UserButton afterSwitchSessionUrl="/" />
+        <UserButton afterSwitchSessionUrl="/" />
       </div>
-      <h1 className="text-4xl font-bold">Hello, World!</h1>
     </div>
   );
 }
