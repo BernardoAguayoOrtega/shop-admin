2e8bd3e8ffa25240d026217bcdeb220eca7bf462
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-10-01 20:41:34 -0600

    Refactor StoreModal component and add form validation

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,44 @@
 "use client";
 
+import * as React from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Missing Name")
+    .min(3, "Name is too short")
+    .max(255, "Name is too long"),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
 
   return (
     <Modal
@@ -12,7 +46,33 @@ export const StoreModal = () => {
       description='Create a new store'
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}>
-      Future Create Store Form
+      <div className='space-y-4 py-2 pb-4'>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='E-Commerce' {...field} />
+                    </FormControl>
+                    <FormMessage>
+                      {form.formState.errors.name?.message}
+                    </FormMessage>
+                  </FormItem>
+                );
+              }}
+            />
+            <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+              <Button variant='outline'>Cancel</Button>
+              <Button type='submit'>Create</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 };
